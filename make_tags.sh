f5444b52e26f89556bbe9345fb04fd8cf9b2dde6
James Whong <james.whong@gmail.com>
2019-06-04 17:06:56 -0700

    Add utility script for making tags

---
+++ make_tags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm tags
+touch unsorted
+find . -name "*" -not -path "./chibios/*" | xargs ctags -a unsorted
+ctags -a unsorted -R chibios/ext
+ctags -a unsorted -R chibios/os/hal/platforms/STM32F4xx
+ctags -a unsorted -R chibios/os/hal/src
+ctags -a unsorted -R chibios/os/hal/include
+sort unsorted >> tags
+rm unsorted
+export VIM_TAGS_FILE=`pwd`/tags
+
