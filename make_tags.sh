c6c5b19bee8399c41b8b2deefdde6a9ccd06c6a0
James Whong <james.whong@gmail.com>
2019-06-05 09:48:08 -0700

    Improve tags

--- make_tags.sh
+++ make_tags.sh
@@ -3,6 +3,7 @@ rm tags
 touch unsorted
 find . -name "*" -not -path "./chibios/*" | xargs ctags -a unsorted
 ctags -a unsorted -R chibios/ext
+ctags -a unsorted -R chibios/os/hal/platforms/STM32
 ctags -a unsorted -R chibios/os/hal/platforms/STM32F4xx
 ctags -a unsorted -R chibios/os/hal/src
 ctags -a unsorted -R chibios/os/hal/include
